ec901fb610f22e629e6d7d198c8c6192993c70a6
Vamsi <uvamsi76@gmail.com>
2023-07-29 08:13:12 +0530

    fresh start

--- backend/src/components/user.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const router=express.Router(); 
-router.get('/courses',(req,res,next)=>{
-    let courses=
-    res.send(200).json()
-})
--- backend/src/router/userroutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.use('/get',(req,res,next)=>{
-    return res.send({"message":"route working"});
-})
-
-export default router;
